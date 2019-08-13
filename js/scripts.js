43b2c4f1bee2acd382e544f22bbdd505d8ee877f
Bahay Gulle Bilgi and Misaki Koonce <bahay@>
2019-08-13 10:33:30 -0700

    Add slideToggle effect.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-  $(".clickable").click(function(){
-    $("#walrus-showing").fadeIn("slow");
-    $("#walrus-showing").fadeOut("slow");
-  });
+  // $(".clickable").click(function(){
+  //   $("#walrus-showing").fadeIn("slow");
+  //   $("#walrus-showing").fadeOut("slow");
+  // });
+  //
+  // $(".clickable2").click(function(){
+  //   $("#dog-showing").slideDown("slow");
+  //   $("#dog-showing").slideUp("slow");
+  // });
 
-  $(".clickable2").click(function(){
-    $("#dog-showing").slideDown("slow");
-    $("#dog-showing").slideUp("slow");
-  });
+    $(".clickable").click(function(){
+      $("#walrus-showing").slideToggle("slow");
+    })
 });
